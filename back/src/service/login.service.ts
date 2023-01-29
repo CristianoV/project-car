e8a3b14ca0bfad8c5a87956c4ea9388be94f2368
CristianoV <cristianoviieira@gmail.com>
2023-01-29 03:22:48 -0300

    feat: service de login criado

---
+++ back/src/service/login.service.ts
@@ -0,0 +1,38 @@
+import JwtSecret from '../utils/JwtService';
+import User from '../database/models/user';
+import Bcrypt from '../utils/BcriptService';
+import { ILoginData, loginSchema } from '../interface/IData/ILoginData';
+
+export default class LoginService {
+  constructor(private model: typeof User) {}
+
+  public async login({ password, name }: ILoginData) {
+    const parsed = loginSchema.safeParse({ name, password });
+
+    if (!parsed.success) {
+      const { message } = parsed.error;
+      const customMessage = JSON.parse(message);
+
+      throw new Error(customMessage[0].message);
+    }
+
+    const user = await this.model.findOne({
+      where: {
+        name,
+      },
+      raw: true,
+    });
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const verifyPassword = Bcrypt.compare(user.password, password);
+
+    if (!verifyPassword) throw new Error('Incorrect email or password');
+
+    const token = JwtSecret.sign({ id: user.id, name: user.name });
+
+    return { token };
+  }
+}
