2c49f6a07989a9b3cd38ec77a2f43c8840c7dcb4
CristianoV <cristianoviieira@gmail.com>
2023-01-29 03:20:33 -0300

    feat: controller de registro criado

---
+++ back/src/database/models/account.ts
@@ -0,0 +1,30 @@
+import {  INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+import User from './user';
+
+class Account extends Model {
+  public id!: number;
+  public balance!: number;
+}
+
+Account.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+    references: { model: 'Users', key: 'accountId' },
+  },
+  nivel: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'Accounts',
+  timestamps: false,
+});
+
+Account.belongsTo(User, { as: 'user', foreignKey: 'id' });
+
+export default Account;
