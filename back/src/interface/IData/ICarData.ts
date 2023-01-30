5ae193652038bd8f5c1ce93ef7bd408d07a93960
CristianoV <cristianoviieira@gmail.com>
2023-01-30 00:56:11 -0300

    feat: adicionado interface de cars

---
+++ back/src/interface/IData/ICarData.ts
@@ -0,0 +1,30 @@
+import { z } from 'zod';
+
+export const carSchema = z.object({
+  name: z.string({
+    required_error: 'name is required',
+    invalid_type_error: 'name must be a string',
+  }),
+  marca: z.string({
+    required_error: 'marca is required',
+    invalid_type_error: 'marca must be a string',
+  }),
+  value: z.string({
+    required_error: 'value is required',
+    invalid_type_error: 'value must be a number',
+  }),
+  // value: z.number({
+  //   required_error: 'value is required',
+  //   invalid_type_error: 'value must be a number',
+  // }),
+  modelo: z.string({
+    required_error: 'modelo is required',
+    invalid_type_error: 'modelo must be a string',
+  }),
+  foto: z.string({
+    required_error: 'foto is required',
+    invalid_type_error: 'foto must be a string',
+  }),
+});
+
+export type ICarData = z.infer<typeof carSchema>;
