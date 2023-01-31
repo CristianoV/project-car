b652435c77405c0674d9967a332daddb9762fd69
CristianoV <cristianoviieira@gmail.com>
2023-01-31 15:08:10 -0300

    feat: adiciona carros do banco de dados na pagina principal

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,8 +1,27 @@
 import Head from 'next/head';
-import Image from 'next/image';
+import { GetServerSideProps } from 'next';
 import styles from '../styles/Home.module.scss';
+import { fetchFromApi } from '../lib/axios';
+import Image from 'next/image';
+import { useState } from 'react';
 
-export default function Home() {
+interface Car {
+  id: string;
+  name: string;
+  marca: string;
+  modelo: string;
+  value: number;
+  foto: string;
+}
+
+interface HomeProps {
+  cars: Car[];
+}
+
+export default function Home({ cars }: HomeProps) {
+  const [carApi, setCarApi] = useState<Car[]>(
+    cars.sort((a, b) => a.value - b.value)
+  );
   return (
     <>
       <Head>
@@ -18,6 +37,27 @@ export default function Home() {
           />
         </div>
       </main>
+      {carApi.map((car) => {
+        return (
+          <div key={car.id}>
+            <h1>{car.name}</h1>
+            <h2>{car.marca}</h2>
+            <h3>{car.modelo}</h3>
+            <h4>{car.value}</h4>
+            <Image src={car.foto} alt={car.name} width={100} height={100} />
+          </div>
+        );
+      })}
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await fetchFromApi.get('/cars');
+
+  return {
+    props: {
+      cars: data,
+    },
+  };
+};
