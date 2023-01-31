d26dc1ecb394ac4d0ca3ebdf885b29dee566edfc
CristianoV <cristianoviieira@gmail.com>
2023-01-30 22:38:35 -0300

    feat: adiciona componentes iniciais

---
+++ front/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <main>
+        <h1>Carros</h1>
+      </main>
+    </>
+  )
+}
