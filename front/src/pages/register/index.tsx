27175233b702c4cbde1054b11201c9db5e53eee0
CristianoV <cristianoviieira@gmail.com>
2023-01-31 11:30:50 -0300

    feat: adiciona imagem e titulo a pagina de registro

--- front/src/pages/register/index.tsx
+++ front/src/pages/register/index.tsx
@@ -1,6 +1,9 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import styles from './styles.module.scss';
+import Image from 'next/image';
+import registerImage from '../../../public/register-customer.jpg';
+import Head from 'next/head';
 
 export default function Register() {
   const [user, setUser] = useState('');
@@ -15,29 +18,35 @@ export default function Register() {
   };
 
   return (
-    <section className={styles.container}>
-      <article>
-        <h1>Crie sua conta</h1>
-        <h3>Adicione um usuario e uma senha!</h3>
-        <form onSubmit={handleSubmit}>
-          <input
-            type='text'
-            placeholder='Usuario'
-            value={user}
-            onChange={(e) => setUser(e.target.value)}
-          />
-          <input
-            type='password'
-            placeholder='Senha'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type='submit'>Entrar</button>
-        </form>
-        <h3>
-          Já tem uma conta cadastrada? <Link href='/login'>Conecte-se</Link>
-        </h3>
-      </article>
-    </section>
+    <>
+      <Head>
+        <title>Registre-se | Faça a sua conta!</title>
+      </Head>
+      <section className={styles.container}>
+        <Image src={registerImage} alt='Login' />
+        <article>
+          <h1>Crie sua conta</h1>
+          <h3>Adicione um usuario e uma senha!</h3>
+          <form onSubmit={handleSubmit}>
+            <input
+              type='text'
+              placeholder='Usuario'
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+            <input
+              type='password'
+              placeholder='Senha'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type='submit'>Entrar</button>
+          </form>
+          <h3>
+            Já tem uma conta cadastrada? <Link href='/login'>Conecte-se</Link>
+          </h3>
+        </article>
+      </section>
+    </>
   );
 }
