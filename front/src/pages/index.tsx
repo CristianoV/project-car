d38d5f3983476934014cca54db10fbca6f0c89d6
CristianoV <cristianoviieira@gmail.com>
2023-01-30 23:26:29 -0300

    feat: adiciona input de pesquisa na pagina inicial

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,16 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Carros usados | Comprar carros usados</title>
       </Head>
-      <main>
-        <h1>Carros</h1>
+      <main className={styles.container}>
+        <div>
+          <input
+            type='text'
+            name=''
+            id=''
+            placeholder='Busque por marca ou modelo'
+          />
+        </div>
       </main>
     </>
-  )
+  );
 }
