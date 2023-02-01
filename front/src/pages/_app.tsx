1de02060b6b268ded56116e9d616e4dfd23d3364
CristianoV <cristianoviieira@gmail.com>
2023-02-01 13:41:54 -0300

    feat: adiciona props para o Header

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,13 +1,12 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
-
-import Header from '../components/Header'
+import type { AppProps } from 'next/app';
+import '../styles/globals.scss';
+import Header from '../components/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
+      <Header {...pageProps} />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
