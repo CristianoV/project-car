266f3aaf537def7e510718e2163769daf5297cdf
CristianoV <cristianoviieira@gmail.com>
2023-02-01 22:00:22 -0300

    feat: implementa verify no index de route

--- back/src/route/index.ts
+++ back/src/route/index.ts
@@ -1,11 +1,13 @@
 import login from './login.route';
 import register from './register.route';
 import car from './car.route';
+import verify from './verify.route';
 
 const routes = [
   login,
   register,
   car,
+  verify,
 ];
 
 export default routes;
