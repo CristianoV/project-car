49506ed5abfd2ecc076d7f964b53caed1a0a01bb
CristianoV <cristianoviieira@gmail.com>
2023-02-02 01:55:52 -0300

    feat: botão de logout adicionado

--- front/src/components/buttonLogin/index.tsx
+++ front/src/components/buttonLogin/index.tsx
@@ -1,17 +1,36 @@
 import Link from 'next/link';
-import { useState } from 'react';
+import { useRouter } from 'next/router';
 import { RiAccountCircleLine } from 'react-icons/ri';
+import { BiLogOut } from 'react-icons/bi';
 
 interface ButtonLoginProps {
   token: string | undefined;
 }
 
 export default function ButtonLogin({ token }: ButtonLoginProps) {
+  const router = useRouter();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    await fetch('/api/logout', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({}),
+    });
+    router.push('/');
+  };
   return token ? (
-    <Link href='/perfil'>
-      <RiAccountCircleLine />
-      Perfil
-    </Link>
+    <>
+      <Link href='/perfil'>
+        <RiAccountCircleLine />
+        Perfil
+      </Link>
+      <Link href='/perfil' onClick={(e) => handleSubmit(e)}>
+        <BiLogOut />
+      </Link>
+    </>
   ) : (
     <Link href='/login'>
       <RiAccountCircleLine />
