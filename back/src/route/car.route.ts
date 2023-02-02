4b1ed47bf32855ebb6dd9b0fa67ef1d1ac86ab38
CristianoV <cristianoviieira@gmail.com>
2023-02-02 09:18:27 -0300

    feat: implementa middleware deleteFile nas rotas

--- back/src/route/car.route.ts
+++ back/src/route/car.route.ts
@@ -2,7 +2,7 @@ import { Request, Response, Router } from 'express';
 import CarModel from '../database/models/car';
 import CarService from '../service/cars.service';
 import CarController from '../controller/car.controller';
-
+import FileMiddleware from '../middleware/File.middleware';
 import upload from '../utils/picture';
 
 const CarRoutes: Router = Router();
@@ -17,16 +17,26 @@ CarRoutes.get('/cars/:id', (request: Request, response: Response) =>
   carController.getCarById(request, response)
 );
 
-CarRoutes.post('/cars', upload.single('file'), (request: Request, response: Response) =>
-  carController.createCar(request, response)
+CarRoutes.post(
+  '/cars',
+  upload.single('file'),
+  (request: Request, response: Response) =>
+    carController.createCar(request, response)
 );
 
-CarRoutes.put('/cars/:id',upload.single('file'), (request: Request, response: Response) =>
-  carController.updateCar(request, response)
+CarRoutes.put(
+  '/cars/:id',
+  upload.single('file'),
+  FileMiddleware.deleteFile,
+  (request: Request, response: Response) =>
+    carController.updateCar(request, response)
 );
 
-CarRoutes.delete('/cars/:id', (request: Request, response: Response) =>
-  carController.deleteCar(request, response)
+CarRoutes.delete(
+  '/cars/:id',
+  FileMiddleware.deleteFile,
+  (request: Request, response: Response) =>
+    carController.deleteCar(request, response)
 );
 
 export default CarRoutes;
