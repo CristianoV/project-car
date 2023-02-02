4d2650f1b0e8a41517bd3c02995a8b8077404ded
CristianoV <cristianoviieira@gmail.com>
2023-02-02 20:14:14 -0300

    feat: adiciona middleware de update para delete de file

--- back/src/middleware/File.middleware.ts
+++ back/src/middleware/File.middleware.ts
@@ -14,9 +14,42 @@ class FileMiddleware {
 
     if (!car) {
       throw new Error('Car not found');
+    } else if (!req.file) {
+      let parts = car.foto.split('files/');
+      let result = parts.slice(-1)[0];
+
+      fs.unlink(`upload/${result}`, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+      });
+
+      next();
+    } else {
+      next();
+    }
+  }
+
+  static async deleteUpdateFile(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const { id } = req.params;
+
+    const car = await Car.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!car) {
+      throw new Error('Car not found');
     } else if (req.file) {
       let parts = car.foto.split('files/');
       let result = parts.slice(-1)[0];
+
       fs.unlink(`upload/${result}`, (err) => {
         if (err) {
           console.error(err);
