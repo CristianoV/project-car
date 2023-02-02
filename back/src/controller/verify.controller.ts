a88df3aa8d64d2aff036dff15e117f43b7ea266f
CristianoV <cristianoviieira@gmail.com>
2023-02-01 22:00:06 -0300

    feat: adiciona controller de verify

---
+++ back/src/controller/verify.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import VerifyService from '../service/verify.service';
+
+export default class VerifyController {
+  constructor(private verifyService: VerifyService) {}
+
+  public async verifyUserController(req: Request, res: Response) {
+    const { authorization } = req.headers as { authorization: string };
+
+    const user = await this.verifyService.verifyUserService(authorization);
+
+    return res.status(202).json(user);
+  }
+}
