ab6d77fa1e92c759e23bb5359a277b33275ec6d6
CristianoV <cristianoviieira@gmail.com>
2023-02-03 11:28:59 -0300

    feat: adiciona mocks da rota verify

---
+++ back/src/tests/mocks/VerifyMocks.ts
@@ -0,0 +1,11 @@
+export const CORRECT_USER =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OTk5LCJuYW1lIjoiQ3Jpc3RpYW5vViIsImlhdCI6MTY3NTQzMzA4NSwiZXhwIjoxNjc1NTE5NDg1fQ.wjCHIy-ylfBFveclQpSpRdH9sduHI7Ak7fHieanKNLk';
+
+export const WRONG_USER =
+  'eyJhbGciOiJIUzI1NiIsInR5cCIpXVCJ9.eyJpZCI6MSwibmFtZSI6ImFzdG9sZm8iLCJpYXQiOjE2NzU0MzM3NDksImV4cCI6MTY3NTUyMDE0OX0.FsSWAmGdVxYgpy_9aiPW6B_nyq_eaI1ASEGi6YRHsaE';
+
+export const USER = {
+  id: 999,
+  nivel: 'admin',
+  user: { name: 'CristianoV' },
+};
