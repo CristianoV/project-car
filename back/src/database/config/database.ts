74a9f73e3fa7d923c25e4caab9e5faa2ff5332d7
CristianoV <cristianoviieira@gmail.com>
2023-02-03 09:05:00 -0300

    feat: altera nome de variavel de ambientes

--- back/src/database/config/database.ts
+++ back/src/database/config/database.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.PG_USER,
-  password: process.env.PG_PASS,
-  database: process.env.PG_DB,
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT),
   dialect: 'postgres',
   dialectOptions: {
     timezone: 'Z',
