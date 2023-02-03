9a982bc33c14de04318460687a6849386d6aa457
CristianoV <cristianoviieira@gmail.com>
2023-02-03 11:34:41 -0300

    feat: adiciona mocks dos testes de login

---
+++ back/src/tests/mocks/LoginMocks.ts
@@ -0,0 +1,37 @@
+export const USER = {
+  id: 1,
+  name: 'user',
+  password: '$2a$10$0ec80axjF54kPvZW5KRmSuQjCGK8P7vqJdoTY7KmUJ8NRzcX2v7pS',
+  accountId: 1
+}
+
+export const LOGIN = {
+  name: 'user',
+  password: 'Abacaxi123',
+};
+
+export const USERNAME_WRONG = {
+  name: 'wrong',
+  password: 'SenhaBemSegura123',
+};
+
+export const PASSWORD_WRONG = {
+  name: 'user',
+  password: 'Wr0ngPassword',
+};
+
+export const SMALL_USERNAME = {
+  name: 'Ci',
+  password: 'SenhaBemSegura123',
+};
+
+export const SMALL_PASSWORD = {
+  name: 'user',
+  password: 'Ab',
+};
+
+export const WRONG_ALPHANUMERIC_PASSWORD = {
+  name: 'user',
+  password: 'senha_sem_validacao',
+};
+
