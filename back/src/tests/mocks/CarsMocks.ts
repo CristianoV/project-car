427c0e6fab287d7d683a059797781f4d6a39d52e
CristianoV <cristianoviieira@gmail.com>
2023-02-03 13:45:53 -0300

    feat: adiciona mock de carros

---
+++ back/src/tests/mocks/CarsMocks.ts
@@ -0,0 +1,26 @@
+export const carsMock = [
+  {
+    id: 993,
+    name: 'Marea',
+    marca: 'Chevrolet',
+    value: 100000,
+    modelo: '1998',
+    foto: 'http://localhost:3002/files/fiat-marea.jpg',
+  },
+  {
+    id: 997,
+    name: 'chevete',
+    marca: 'Chevrolet',
+    value: 700000,
+    modelo: '1998',
+    foto: 'http://localhost:3002/files/chevete.png',
+  },
+  {
+    id: 998,
+    name: 'Monza',
+    marca: 'Chevrolet',
+    value: 800000,
+    modelo: '1998',
+    foto: 'http://localhost:3002/files/monza.jpg',
+  },
+];
